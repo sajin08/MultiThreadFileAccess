11d65f56f01c639e353909e73e56371a3cabeb28
Sajin Maharjan <sajinmhz10@gmail.com>
2024-09-21 13:20:59 -0400

    Update to more object oriented design

---
+++ MultiThreadFileAccess/Interfaces/IProcessor.cs
@@ -0,0 +1,16 @@
+﻿using MultiThreadFileAccess.Exceptions;
+
+namespace MultiThreadFileAccess.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal interface IProcessor
+    {
+        /// <summary>
+        /// Will run the process and repeat the action
+        /// </summary>
+        /// <param name="occurences">number of time to repeat the action</param>
+        Task RunAsync(int occurences);
+    }
+}
