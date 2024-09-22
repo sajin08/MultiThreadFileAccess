53025c1a40135860cb68a17e87e6a76231e631ff
Sajin Maharjan <sajinmhz10@gmail.com>
2024-09-21 22:58:38 -0400

    Added comments for more details

--- MultiThreadFileAccess/FileHelper.cs
+++ MultiThreadFileAccess/FileHelper.cs
@@ -1,7 +1,15 @@
 ﻿namespace MultiThreadFileAccess
 {
+    /// <summary>
+    /// FileHelper for operations related to file
+    /// </summary>
     public class FileHelper
     {
+        /// <summary>
+        /// A write method that writes content to a file specified by the filePath parameter.
+        /// </summary>
+        /// <param name="filePath">file path to write to</param>
+        /// <param name="content">content for the file</param>
         public static void WriteToFile(string filePath, string content)
         {
             using (StreamWriter writer = new StreamWriter(filePath, true))
--- MultiThreadFileAccess/Program.cs
+++ MultiThreadFileAccess/Program.cs
@@ -3,6 +3,7 @@ using MultiThreadFileAccess;
 
 class Program
 {
+    //Initialized local variables for the application
     private static readonly object fileLock = new object();
     private static readonly string fileDirectory = "log/";
     private static readonly string fileName = "log.txt";
@@ -10,22 +11,27 @@ class Program
 
     static async Task Main(string[] args)
     {
+        //create directory if does not exists
         if (!Directory.Exists(fileDirectory))
             Directory.CreateDirectory(fileDirectory);
+        //initialize new file with default text
         using (var fileStream = File.CreateText(fileDirectory + fileName))
         {
             fileStream.WriteLine($"{counter++}, 0, {DateTime.Now:HH:MM:ss.mmm}");
         }
+        //create list of task to execute in parallel
         var tasksToExecuteInParallel = new List<Task>();
 
-        // Create and start multiple threads
+        // add new tasks to tasksToExecuteInParallel to execute all the task at once
         for (int i = 0; i < 10; i++)
         {
             tasksToExecuteInParallel.Add(Task.Run(() => WriteToFile()));
         }
 
+        //Execute all the task and wait for all task to be complet
         await Task.WhenAll(tasksToExecuteInParallel);
 
+        //Pause for user input to exit application
         Console.ReadLine();
     }
 
@@ -34,10 +40,13 @@ class Program
         var threadId = Thread.CurrentThread.ManagedThreadId;
         try
         {
+            //create 10 new rows for each thread
             for (int i = 0; i < 10; i++)
             {
+                //use lock to ensure file is being access by one thread at a time for write
                 lock (fileLock)
                 {
+                    //pass file path and file content to WriteToFile method to append in the file
                     FileHelper.WriteToFile(fileDirectory + fileName, $"{counter++}, {threadId}, {DateTime.Now:HH:MM:ss.mmm}");
                 }
             }
